016f6cfbdd09f212970bc655d6d129a9aa1c86d2
OL-WG <47134274+OL-WG@users.noreply.github.com>
2026-03-10 01:16:17 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
 let tg = window.Telegram.WebApp;
 tg.expand();
-
-// Красим интерфейс самого Telegram в черный
 tg.setHeaderColor('#000000');
 tg.setBackgroundColor('#000000');
 
@@ -27,11 +25,8 @@ function updateCartUI() {
 
 function sendOrder() {
     if (cart.length === 0) return;
-    
     let items = cart.map(item => item.name).join(", ");
     let total = cart.reduce((sum, item) => sum + item.price, 0);
-    
-    // Отправка данных боту
     tg.sendData(`🛒 Заказ: ${items} на сумму ${total}₽`);
     tg.close();
 }
